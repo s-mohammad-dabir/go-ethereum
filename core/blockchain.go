7f3081efbbab425b48c019e36bed30295048ff78
Seyed Mohammad Dabir <20028160+s-mohammad-dabir@users.noreply.github.com>
2023-10-22 16:17:29 +0330

    Increase trie storage count

--- core/blockchain.go
+++ core/blockchain.go
@@ -103,7 +103,7 @@ const (
 	txLookupCacheLimit  = 1024
 	maxFutureBlocks     = 256
 	maxTimeFutureBlocks = 30
-	TriesInMemory       = 128
+	TriesInMemory       = 10000
 
 	// BlockChainVersion ensures that an incompatible database forces a resync from scratch.
 	//
